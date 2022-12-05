228e19cba47bb2a6b85d73afff18da7e9b2cdb67
Kimjiwon <hghg1036@naver.com>
2022-12-05 12:14:08 +0900

    Security 초기세팅완료

---
+++ src/main/java/com/example/jwt/config/SecurityConfig.java
@@ -0,0 +1,53 @@
+package com.example.jwt.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.example.jwt.config.SecurityConfig.MyCustomDsl;
+
+
+// 현재 상태 : 세션을 사용하지 않고 있고 시큐리티를 직접 만들었고 현재로서는 모든 페이지에 접근이 가능하다
+@Configuration
+@EnableWebSecurity // 시큐리티 활성화 -> 기본 스프링 필터체인에 등록
+public class SecurityConfig {
+
+	@Autowired
+	private CorsConfig corsConfig;
+
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		return http
+				.csrf().disable()
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션 사용 안함
+				.and()
+				.formLogin().disable() // jwt 서버이기 때문에 form태그 로그인 방식 사용X
+				.httpBasic().disable() // ==== 여기까지는 고정
+				.apply(new MyCustomDsl()) // 커스텀 필터 등록 
+				.and()
+				.authorizeRequests(authroize -> authroize.antMatchers("/api/v1/user/**") // 이 주소로
+				.access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+						// user, manager, admin 가능
+						.antMatchers("/api/v1/manager/**")
+						.access("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+						.antMatchers("/api/v1/admin/**")
+						.access("hasRole('ROLE_ADMIN')")
+						.anyRequest().permitAll()) // 다른 요청은 권한없이 들어갈 수 있다
+				.build();
+	}
+
+	public class MyCustomDsl extends AbstractHttpConfigurer<MyCustomDsl, HttpSecurity> {
+		@Override
+		public void configure(HttpSecurity http) throws Exception {
+			AuthenticationManager authenticationManager = http.getSharedObject(AuthenticationManager.class);
+			http.addFilter(corsConfig.corsFilter());
+		}
+	}
+
+}
